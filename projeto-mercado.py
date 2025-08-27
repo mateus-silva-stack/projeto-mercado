ac4ced04cffc64313d483b275f8258b75d54175d
Mateus Aurélio <mateus.aurelio@gmail.com>
2025-08-27 19:50:44 -0300

    Atualização com banco de dados funcionando

--- projeto-mercado.py
+++ projeto-mercado.py
@@ -1,19 +1,23 @@
-banco_de_alimentos = {
+import json
+
+banco = {
     "Arroz": { "preço" : 19.90 , "data_validade" :  "28/01/2025" },
     "Feijao" : { "preço" : 11.99 , "data_validade" : "23/03/2025" },
     "Açucar" : { "preço" : 8.99 , "data_validade" : "26/06/2025" },
     "Macarrao" : { "preço" : 13.99 , "data_validade" : "12/06/2025" },
     "oleo" : { "preço" : 14.99 , "data_validade" : "30/06/2025" },
 }
-while True:
-    produto = input("digite qual o produto você deseja: ").lower()
 
-    if produto == "sair":
-        print("Encerrando sistema...")
-        break
-    elif produto in banco_de_alimentos:
-        print(banco_de_alimentos[produto])
+with open ("banco.json","w") as arquivo:
+    json.dump(banco, arquivo, indent=4 )
 
+with open("banco.json", "r") as arquivo:
+    dados = json.load(arquivo)
+while True: 
+    produto = input("qual produto você deseja consultar? ")
+
+    if produto in dados:
+        print(dados[produto])
+        
     else:
-        print("Produto não encontrado!")
-  
+       print("Produto não encontrado")
